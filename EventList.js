fa5ad319433fd90404403ab14166f62719f476ec
Svetoslav Savov <svetoslav.savov@stanga.net>
2019-11-10 13:25:16 +0200

    Initial connit

---
+++ EventList.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    FlatList
+} from "react-native";
+import ActionButton from 'react-native-action-button';
+
+import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#F3F3F3'
+    },
+});
+
+class EventList extends Component {
+    state = {
+        events: []
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                events: this.state.events.map(evt => ({
+                    ...evt,
+                    timer: Date.now()
+                }))
+            })
+        }, 1000);
+        const events = require('./db.json').events.map(e => ({
+            ...e,
+            date: new Date(e.date),
+        }));
+        this.setState({ events });
+    }
+
+    handleAddEvent = () => {
+        this.props.navigation.navigate('form');
+    }
+
+    render() {
+        return [
+            <FlatList
+                key = "flatlist"
+                style={styles.list}
+                data={this.state.events}
+                renderItem={({ item }) => <EventCard event={item} />}
+                keyExtractor={item => item.id}
+            />,
+            <ActionButton 
+                key="fab"
+                buttonColor = "rgba(231,76,60,1)"
+                onPress={this.handleAddEvent}
+            />
+        ]
+    }
+}
+
+export default EventList;
